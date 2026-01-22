1d1f7fc6773eb1b5b898e37558adb8caa9223818
nithya7407-codes <nithya7407@gmail.com>
2026-01-22 14:55:38 +0530

    Delete exercise 3 directory

--- exercise 3/ForLoop.java
+++
@@ -1,7 +0,0 @@
-class ForLoop {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-    }
-}
--- exercise 3/SimpleIf.java
+++
@@ -1,9 +0,0 @@
-class SimpleIf {
-    public static void main(String[] args) {
-        int age = 18;
-
-        if (age >= 18) {
-            System.out.println("Eligible to vote");
-        }
-    }
-}
